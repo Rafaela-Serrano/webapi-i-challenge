9c50c96d95dc14eebc0a75c4aa5aaf00cd144710
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-07-20 13:46:34 -0500

    Created a express app that we use to configure a server and responso to a get request creating multiple endpoints

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require('express');//import express from 'express';
+const database = require('./data/db');
+
+
+const server = express();// creates and Express app we use to configure server 
+
+// "/" -> this is the URL that will trigger the get request , req and res argument are and object with methods and properties
+
+server.get('/', (req, res) => { 
+//this function is the request handler function 
+    res.send('Hello World!')
+}); // this is a configure of and endpoint 
+
+server.get('/hobbits', (req, res) => {
+    // create a list of hobbits 
+    const hobbits = [
+        { id:1, name: 'Samwise Gamgee' },
+        { id:2, name: 'Frodo Baggins' },
+    ];
+
+    // http response status code 
+
+    res.status(200).json(hobbits);
+    
+})
+
+server.get('/users', (req, res) => {
+
+    database.find()
+    .then( resp => res.status(200).json(resp))
+    .catch( error => res.status(500).json(error))
+})
+
+server.listen(8000, () => console.log('API running on port 8000'))
